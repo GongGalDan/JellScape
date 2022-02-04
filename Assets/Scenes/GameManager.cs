714d1817e1e1b0b39fd27edb6a23c44d0afab05f
Chanwoongs <tkfkdcksdnd@daum.net>
2022-02-04 14:57:53 +0900

    싱글톤을 이용하여 GameManager 추가

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : InheritSingleton<GameManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
